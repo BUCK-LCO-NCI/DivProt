a0cf21a91dabc4d4df98db7bcce27e07ea99c941
Anna Katherine Belford <belfordak@biowulf.nih.gov>
2019-02-04 10:50:49 -0500

    adding current versions of py aligner and R vis scripts

---
+++ R_script_strct.R
@@ -0,0 +1,97 @@
+#!/usr/bin/env r
+
+#produce matrices
+
+library("tidyr")
+library("dplyr")
+library("reshape")
+
+##subject file
+struct_align_score_table <- read.table("test_out.csv", sep = "\t", header = FALSE)
+
+colnames(struct_align_score_table) <- c("qseqid", "sseqid", "align_score")
+
+struct_align_score_table$align_score = as.numeric(gsub("Score=", "", struct_align_score_table$align_score)) #gets rid of Score=
+
+####align score
+shaped_algn_score <- reshape(struct_align_score_table,idvar = "qseqid", timevar = "sseqid", direction = "wide")
+names(shaped_algn_score) <- substring(names(shaped_algn_score),13,100) #...don't rmember what 10,100 does....
+
+
+#this creates rownames (seq ids are in a  column/row now)
+tmpb <- shaped_algn_score[,-1]
+rownames(tmpb) <- shaped_algn_score[,1]
+shaped_algn <- tmpb
+
+algnscore_matrix <- shaped_algn_score[,rownames(shaped_algn_score)]
+
+
+#Replaces NA with 0, was necessary for amino acid script as perfect alignments gave us NA out. Here perfect alignment across the diagonal = 180. Not a problem (pretty sure) as we can ignore diagonal 
+#algnscore_matrix[is.na(algnscore_matrix)] <- 0 #matrix with 0s to replace Nas
+
+
+write.csv(algnscore_matrix, "algnscore_matrix.csv") 
+
+
+#dendrogram / tree   
+library("stats")
+pdf(file = "Rplots.pdf")
+for_hclust <- dist(algnscore_matrix, method = "canberra", diag = FALSE, upper = FALSE, p = 2)
+hc <- hclust(for_hclust, method = "average", members = NULL)
+plot(hc, hang = -0.5, cex = 0.6) 
+
+#more trees
+library("ape")
+##unrooted
+myphylo <- as.phylo.hclust(hc)
+plot(as.phylo.hclust(hc), type = "unrooted", cex = 0.6,
+     no.margin = TRUE)
+
+##Fan/circular
+plot(as.phylo.hclust(hc), type = "fan")
+
+#heatmap
+library("pheatmap")
+ph <- pheatmap(algnscore_matrix, width = 10, height = 10)
+plot(ph, cex = 0.6)
+
+
+#igraph, and heatmap, weirdly enough
+matrix_confirm <- as.matrix(algnscore_matrix)
+mode(matrix_confirm) <- "numeric"
+#plot(matrix_confirm, cex = 0.6) #this turned into the heatmap once...
+
+
+#actual igrpah, two for different layout options 
+ig <- graph.adjacency(matrix_confirm, mode="undirected", weighted=TRUE, diag = TRUE)
+
+community_clustering <- fastgreedy.community(ig)
+cluster_colors <- rainbow(max(membership(community_clustering)))
+
+l <- layout <- layout.reingold.tilford(ig, circular=T)
+ll <- layout.fruchterman.reingold(ig, niter=10000)
+
+plot(ig, layout=l, 
+     edge.arrow.size=0.5, 
+     vertex.label.cex=0.5, 
+     vertex.label.family="Helvetica",
+     vertex.label.font=2,
+     vertex.shape="circle", 
+     vertex.size=3,
+     vertex.color=colors[membership(fc)], 
+     vertex.label.color="black", 
+     edge.width=0.4)
+
+plot(ig, layout=ll, 
+     edge.arrow.size=0.5, 
+     vertex.label.cex=0.5, 
+     vertex.label.family="Helvetica",
+     vertex.label.font=2,
+     vertex.shape="circle", 
+     vertex.size=3,
+     vertex.color=colors[membership(fc)], 
+     vertex.label.color="black", 
+     edge.width=0.4)
+
+dev.off()
+
