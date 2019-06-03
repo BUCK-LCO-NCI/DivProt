fe8ec761ab066bcc8d9af6c62ee3b578b1f59eae
Anna <anna.k.belford@gmail.com>
2019-06-03 14:12:33 -0400

    Wrapper for individual R scripts

---
+++ R_figs_wrapper.py
@@ -0,0 +1,20 @@
+
+#python3 run_figures.py -tsne_trees/heatmap/network -k (or -tm for threshold modifier, but actually doesn't matter, just change so people arent confusd) 2
+import subprocess
+
+
+script_to_pass = sys.argv[2]
+
+#this loop if for deining deafult values if -k/-tm are not given 
+if script_to_pass == "-tsne_trees":
+    variable_to_pass = sys.argv[4] if len(sys.argv) >= 4 else '1'
+elif scrip t_to_pass == "-network":
+    variable_to_pass = sys.argv[4] if len(sys.argv) >= 4 else #'the default cutoff soon-to-come'
+
+
+if script_to_pass == "-heatmap":
+    subprocess.check_call(['Rscript', 'figures_1_heatmap.R'], stderr=subprocess.STDOUT, shell=False) #or just Rscript figures_1_heatmap.R
+elif script_to_pass == "-tsne_trees":
+    subprocess.check_call(['Rscript', '--args', '$variable_to_pass','figures_2_tsne_trees.R'], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
+elif script_to_pass == "-network":
+    subprocess.check_call(['Rscript', '--args', '$variable_to_pass','figures_3_network.R'], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
