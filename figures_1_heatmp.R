8fbd5895cb848665ba0a36dcf36f957376069f1c
Anna <anna.k.belford@gmail.com>
2019-06-05 09:43:40 -0400

    Update figures_1_heatmp.R

--- figures_1_heatmp.R
+++ figures_1_heatmp.R
@@ -3,8 +3,8 @@ library("pheatmap")
 
 pdf(file = "heatmap_Rplots1.pdf")
 
-algnscore_matrix <- read.csv("./algnscore_matrix.csv") #remeber this matrix needs to be fully done w/ R script before this vis
-
+#algnscore_matrix <- read.csv("./algnscore_matrix.csv") #remeber this matrix needs to be fully done w/ R script before this vis
+algnscore_matrix <-  align_file #var passed from the python wrapper script
 matrix_confirm <- as.matrix(algnscore_matrix)
 mode(matrix_confirm) <- "numeric"
 pheatmap(shaped_algn, cex = 0.7, main = "Heatmap")
