1d303d7f46e8d1362ba7a0d3444baf1df88f3099
Anna <anna.k.belford@gmail.com>
2019-06-05 09:36:36 -0400

    Update R_figs_wrapper.py

--- R_figs_wrapper.py
+++ R_figs_wrapper.py
@@ -6,16 +6,17 @@ import sys
 align_file = sys.argv[1]
 script_to_pass = sys.argv[2]
 
+
 #this loop if for deining deafult values if -k/-tm are not given 
 if script_to_pass == "-tsne_trees":
-    variable_to_pass = sys.argv[4] if len(sys.argv) >= 4 else '1'
+    var_score_to_pass = sys.argv[4] if len(sys.argv) >= 4 else '1'
 elif scrip t_to_pass == "-network":
-    variable_to_pass = sys.argv[4] if len(sys.argv) >= 4 else #'the default cutoff soon-to-come'
+    var_score_to_pass = sys.argv[4] if len(sys.argv) >= 4 else #'the default cutoff soon-to-come'
 
 
 if script_to_pass == "-heatmap":
     subprocess.check_call(['Rscript', 'figures_1_heatmap.R'], stderr=subprocess.STDOUT, shell=False) #or just Rscript figures_1_heatmap.R
 elif script_to_pass == "-tsne_trees":
-    subprocess.check_call(['Rscript', '--args', '$variable_to_pass','figures_2_tsne_trees.R'], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
+    subprocess.check_call(['Rscript', '--args', '$align_file','$var_score_to_pass','figures_2_tsne_trees.R'], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
 elif script_to_pass == "-network":
-    subprocess.check_call(['Rscript', '--args', '$variable_to_pass','figures_3_networks.R'], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
+    subprocess.check_call(['Rscript', '--args', '$var_score_to_pass','figures_3_networks.R'], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
