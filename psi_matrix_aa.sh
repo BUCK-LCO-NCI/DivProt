a21daa2ffe887f4872096119bf6accbb3f55a2a2
Anna <anna.k.belford@gmail.com>
2019-07-09 18:49:36 -0400

    Update psi_matrix_aa.sh

--- psi_matrix_aa.sh
+++ psi_matrix_aa.sh
@@ -2,7 +2,7 @@
 
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
+exec 1>log_psi_aa.out 2>&1
 
 module load blast
 module load R
@@ -18,4 +18,5 @@ cd $DIRNAME
 
 psiblast -query "../$QUERYFILE" -subject "../$SUBJECTFILE" -outfmt 6 > PSIBLAST_results.txt
 
-Rscript R_script_aa.R
+module load R #incase you forgot ;)
+Rscript ../R_script_aa.R
