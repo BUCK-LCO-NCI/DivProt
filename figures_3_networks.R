14c4b5363b0ca80be594df291789d22112be4f4a
Anna <anna.k.belford@gmail.com>
2019-07-17 15:47:08 -0400

    Update figures_3_networks.R

--- figures_3_networks.R
+++ figures_3_networks.R
@@ -18,7 +18,7 @@ community_clustering <- fastgreedy.community(ig)
 cluster_colours <- rainbow(max(membership(community_clustering)), alpha = 0.5)
 
 l <- layout <- layout.reingold.tilford(ig, circular=T)
-ll <- layout.fruchterman.reingold(ig, niter=5000)
+ll <- layout_with_graphopt(ig, niter=5000)
 #layout_nicel  with the same params = same graph. This seems to be the best network layout for this data. If you want to play around with layouts, we highly recommend a high number of iterations like we've used, as it makes a significant difference in clustering. Higher niter = MUCH more accurate
 #circlar plot provided b/c sometimes the characteristics of data can make a typical network hard to read. It's usually not going to be espacially valuable.
 
