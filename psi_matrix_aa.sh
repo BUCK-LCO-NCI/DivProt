19a14fbbdc15cf0595314d067d9430fa11e68709
Anna <anna.k.belford@gmail.com>
2019-07-29 19:05:44 -0400

    Update psi_matrix_aa.sh

--- psi_matrix_aa.sh
+++ psi_matrix_aa.sh
@@ -16,7 +16,8 @@ SUBJECTFILE=$1
 mkdir -p $DIRNAME
 cd $DIRNAME
 
-psiblast -query "../$QUERYFILE" -subject "../$SUBJECTFILE" -outfmt 6 > PSIBLAST_results.txt
-
+psiblast -query "../$QUERYFILE" -subject "../$SUBJECTFILE" -outfmt 6 -evalue 100 > PSIBLAST_results.txt
+#note the VERY liberal e-value. You can change this if you want, but keep in mind that you will not be able to weight with aa results that are incomplete, i.e. you have no blast results for two seqs (different data sizes cannot be merged) 
+#also note though that while e-val 100 is almost comical, in our testing of short (20-55), divergent seqs, even e-val 100 is too strict...
 module load R #incase you forgot ;)
 Rscript ../R_script_aa.R
