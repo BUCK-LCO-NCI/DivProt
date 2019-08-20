3c13cb7016e859a6ee02a1bf3f8d9d58cda3033d
Anna <anna.k.belford@gmail.com>
2019-08-20 12:23:34 -0400

    Update R_figs_wrapper.py

--- R_figs_wrapper.py
+++ R_figs_wrapper.py
@@ -1,6 +1,8 @@
 #python3 run_figures.py alignmatrix.csv -heatmap
 #python3 run_figures.py alignmatrix.csv -tsne_trees -k 2
-#python3 run_figures.py alignmatrix.csv -network original_fasta_name.fa -tm 1.2 
+#python3 run_figures.py alignmatrix.csv -network original_fasta_name.fa 
+#python3 run_figures.py alignmatrix.csv -network_custom original_fasta_name.fa -tm 1.2 
+
 import subprocess
 import sys
 
@@ -12,8 +14,8 @@ orig_fasta = sys.argv[3]
 #this loop if for deining deafult values if -k/-tm are not given 
 if script_to_pass == "-tsne_trees":
     var_score_to_pass = sys.argv[4] if len(sys.argv) >= 4 else '1'
-elif script_to_pass == "-network":
-    var_score_to_pass = sys.argv[4] if len(sys.argv) >= 4 else 'XXXXXX'#'the default cutoff soon-to-come' #NOTEEE TO ME
+elif script_to_pass == "-network_custom":
+    var_score_to_pass = sys.argv[4] if len(sys.argv) >= 5 else 'XXXXXX'#'the default cutoff soon-to-come' #NOTEEE TO ME #>=4?
 
 
 if script_to_pass == "-heatmap":
@@ -21,4 +23,6 @@ if script_to_pass == "-heatmap":
 elif script_to_pass == "-tsne_trees":
     subprocess.call(['Rscript', 'figures_2_tsne_trees.R', align_file, var_score_to_pass], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
 elif script_to_pass == "-network":
-    subprocess.call(['Rscript', 'figures_3_networks.R', align_file, var_score_to_pass, orig_fasta], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
+    subprocess.call(['Rscript', 'figures_3_networks.R', align_file, orig_fasta], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
+elif script_to_pass == "-network_custom":
+    subprocess.call(['Rscript', 'figures_3_networks_custom.R', align_file, var_score_to_pass, orig_fasta], stderr=subprocess.STDOUT, shell=False) #pass fastq variable on
