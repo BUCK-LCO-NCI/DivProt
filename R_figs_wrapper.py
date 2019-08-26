55b4aecb893fe4cc397145777f2de96696bc6226
Anna <anna.k.belford@gmail.com>
2019-08-26 11:04:15 -0400

    fixed heatmap + fasta bug

--- R_figs_wrapper.py
+++ R_figs_wrapper.py
@@ -1,23 +1,24 @@
-#python3 run_figures.py alignmatrix.csv -heatmap
-#python3 run_figures.py alignmatrix.csv -tsne_trees -k 2
-#python3 run_figures.py alignmatrix.csv -network original_fasta_name.fa 
-#python3 run_figures.py alignmatrix.csv -network_custom original_fasta_name.fa -tm 1.2 
+#python3 R_figs_wrapper.py alignmatrix.csv -heatmap
+#python3 R_figs_wrapper.py alignmatrix.csv -tsne_trees -k 2
+#python3 R_figs_wrapper.py alignmatrix.csv -network original_fasta_name.fa 
+#python3 R_figs_wrapper.py alignmatrix.csv -network_custom original_fasta_name.fa -tm 1.2 
 
 import subprocess
 import sys
 
 align_file = sys.argv[1]
 script_to_pass = sys.argv[2]
-orig_fasta = sys.argv[3]
 
-
-#this loop if for deining deafult values if -k/-tm are not given 
-if script_to_pass == "-tsne_trees":
+#definitions
+if script_to_pass == "-network":
+    orig_fasta = sys.argv[3]
+elif script_to_pass == "-tsne_trees":
     var_score_to_pass = sys.argv[4] if len(sys.argv) >= 4 else '1'
 elif script_to_pass == "-network_custom":
+    orig_fasta = sys.argv[3]
     var_score_to_pass = sys.argv[5] #if len(sys.argv) >= 5 else 'XXXXXX' 'the default cutoff soon-to-come' #NOTEEE TO ME #>=4?
 
-
+#var passing to R
 if script_to_pass == "-heatmap":
     subprocess.call(['Rscript', 'figures_1_heatmap.R', align_file], stderr=subprocess.STDOUT, shell=False) #or just Rscript figures_1_heatmap.R
 elif script_to_pass == "-tsne_trees":
