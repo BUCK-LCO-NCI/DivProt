35c11c3bce4909d829609df135433811ac9bf6cb
Anna <anna.k.belford@gmail.com>
2020-02-27 13:52:32 -0500

    Update figures_1_heatmap.R

--- figures_1_heatmap.R
+++ figures_1_heatmap.R
@@ -10,6 +10,6 @@ pdf(file = "heatmap_Rplots1.pdf")
 algnscore_matrix <-  align_file #var passed from the python wrapper script
 matrix_confirm <- as.matrix(algnscore_matrix)
 mode(matrix_confirm) <- "numeric"
-pheatmap(matrix_confirm, cex = 0.7, main = "Heatmap")
+pheatmap(matrix_confirm, cex = 0.5, main = "Heatmap")
 
 dev.off()
