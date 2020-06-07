4a23a17fe125372d1c1816e52647150bae5fb5cc
Anna <anna.k.belford@gmail.com>
2020-06-07 16:55:19 -0400

    Update figures_1_heatmap.R
    added upper triangle selection

--- figures_1_heatmap.R
+++ figures_1_heatmap.R
@@ -1,5 +1,6 @@
 #heatmap
 library("pheatmap")
+library("Matrix")
 
 args = commandArgs(trailingOnly=TRUE)
 align_file = read.csv(args[1], header = TRUE, row.names = 1, check.names = FALSE)
@@ -10,6 +11,11 @@ pdf(file = "heatmap_Rplots1.pdf")
 algnscore_matrix <-  align_file #var passed from the python wrapper script
 matrix_confirm <- as.matrix(algnscore_matrix)
 mode(matrix_confirm) <- "numeric"
-pheatmap(matrix_confirm, cex = 0.5, border_color = NA, main = "Heatmap")
+
+#we are taking the upper triangle of the matrix. As it is not entirely symetrical, the resulting heatmap can be difficult to read (shifted daigonal/greatest value not on the diagonal) 
+upper_only <- forceSymmetric(matrix_confirm, uplo = "U")
+
+
+pheatmap(upper_only, cex = 0.5, border_color = NA, main = "Heatmap")
 
 dev.off()
